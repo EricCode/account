7fbf64597d5549420219b0ed4ae9ba69e5fd224d
ericcode <eric78723@gmail.com>
2016-07-25 17:50:12 +0800

    account, password

--- account/ViewController.swift
+++ account/ViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.account.delegate = self
+        self.password.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == account{
+        password.becomeFirstResponder()
+        }
+        else{
+            self.view.endEditing(true)
+        }
+        return true
     }
 
 
